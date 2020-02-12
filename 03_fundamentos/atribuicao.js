22d7f1a6276a4934a6144700a395de75df8ecbd4
mateus.luz <mateus.luz@gsw.com.br>
2020-02-12 15:34:13 -0300

    simple update

---
+++ 03_fundamentos/atribuicao.js
@@ -0,0 +1,19 @@
+const a = 7
+let b = 3
+
+b += a // atribuição aditiva (junto com a atribuição faz-se a adição), ou seja, b = b + a
+console.log(b)
+
+b -= 4 // atribuição subtrativa, ou seja, b = b - 4
+console.log(b)
+
+b *= 2 // atribuição multiplicativa, ou seja, b = b * 2
+console.log(b)
+
+b /= 2 // atribuição divisiva, ou seja, b = b / 2
+console.log(b)
+
+b %= 2 // atribuição modular, ou seja, b = b % 2
+console.log(b)
+
+// símbolo aritimético antes e a atribuição depois
