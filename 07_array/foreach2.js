607a2fd220c3a2463c32081535bf8f3f36e35831
mateus.luz <mateus.luz@gsw.com.br>
2020-03-04 15:30:50 -0300

    simple update

--- 07_array/foreach2.js
+++ 07_array/foreach2.js
@@ -1,4 +1,8 @@
-
+Array.prototype.forEach2 = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  };
+};
 
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel'];
 
